f89995a2351ec9a442aadae87f9eb66b96cda951
Duy Tran <duyvutran2303@gmail.com>
2021-08-02 18:09:29 -0500

    Added solution for LCS of 2 Strings

--- src/LCS.java
+++ src/LCS.java
@@ -2,6 +2,31 @@ import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class LCS {
+
+  public int lcsOf2(String text1, String text2) {
+    char[] t1 = text1.toCharArray();
+    char[] t2 = text2.toCharArray();
+
+    int m = text1.length();
+    int n = text2.length();
+
+    int[][] dp = new int[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+      for (int j = 0; j <= n; j++) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = 0;
+        } else if (t1[i-1] == t2[j-1]) {
+          dp[i][j] = dp[i-1][j-1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
+
   public int lcsOf3(String X, String Y, String Z) {
     int m = X.length();
     int n = Y.length();
