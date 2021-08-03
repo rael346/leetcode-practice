9be023d5964f3f72f63ad71530123a1a3d69c118
Duy Tran <duyvutran2303@gmail.com>
2021-08-03 02:04:16 -0500

    Added solution for MaxSubarray

--- src/maximum_subarray/MaxSubarray.java
+++ src/maximum_subarray/MaxSubarray.java
@@ -2,4 +2,15 @@ package maximum_subarray;
 
 public class MaxSubarray {
 
+  public int maxSubArray(int[] nums) {
+    int M = nums[0];
+    int m = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      m = Math.max(nums[i], m + nums[i]);
+      M = Math.max(m, M);
+    }
+
+    return M;
+  }
 }
